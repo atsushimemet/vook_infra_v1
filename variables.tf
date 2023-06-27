79af6d35f0bf5d876e844f44b8190bfa4c1b9e40
atsushimemet <noap3b69n@gmail.com>
2023-06-27 10:21:36 +0900

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+# ---------------------------
+# 変数設定
+# ---------------------------
+variable "az_a" {
+  default     = "ap-northeast-1a"
+}
+
+variable "access_key" {
+  default     = "AKIAYHSCV5C25AZ6FJOG"
+}
+
+variable "secret_key" {
+  default     = "5uhuPXaKb6Mi/h39arOy/6hyc0uKvh1Dl532FxRa"
+}
