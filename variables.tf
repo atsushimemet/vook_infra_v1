b16db6d24340721b74d4ecea36ec490ae602267e
atsushimemet <noap3b69n@gmail.com>
2023-06-30 08:50:14 +0900

    fix 誤って記載したiamのキー情報

--- variables.tf
+++ variables.tf
@@ -5,10 +5,6 @@ variable "az_a" {
   default     = "ap-northeast-1a"
 }
 
-variable "access_key" {
-  default     = "AKIAYHSCV5C25AZ6FJOG"
-}
+variable "access_key" {}
 
-variable "secret_key" {
-  default     = "5uhuPXaKb6Mi/h39arOy/6hyc0uKvh1Dl532FxRa"
-}
+variable "secret_key" {}
