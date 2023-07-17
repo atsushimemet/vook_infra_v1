3cdfd50d0bc7f4448c8cb52caa61ad4d0e4cf81a
atsushimemet <noap3b69n@gmail.com>
2023-07-17 14:52:04 +0900

    update vook rails 2に変更

--- dev_2/vpc.tf
+++ dev_2/vpc.tf
@@ -1,44 +1,44 @@
 # ---------------------------
 # VPC
 # ---------------------------
-resource "aws_vpc" "vook_rails_1_vpc" {
+resource "aws_vpc" "vook_rails_2_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true # DNSホスト名を有効化
   tags = {
-    Name = "vook-rails-1-vpc"
+    Name = "vook-rails-2-vpc"
   }
 }
 
 # ---------------------------
 # Subnet
 # ---------------------------
-resource "aws_subnet" "vook_rails_1_public_subnet_1a" {
-  vpc_id            = aws_vpc.vook_rails_1_vpc.id
+resource "aws_subnet" "vook_rails_2_public_subnet_1a" {
+  vpc_id            = aws_vpc.vook_rails_2_vpc.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = var.az_a
 
   tags = {
-    Name = "vook-rails-1-public-subnet-1a"
+    Name = "vook-rails-2-public-subnet-1a"
   }
 }
 
-resource "aws_subnet" "vook_rails_1_private_subnet_1a" {
-  vpc_id            = aws_vpc.vook_rails_1_vpc.id
+resource "aws_subnet" "vook_rails_2_private_subnet_1a" {
+  vpc_id            = aws_vpc.vook_rails_2_vpc.id
   cidr_block        = "10.0.20.0/24"
   availability_zone = var.az_a
 
   tags = {
-    Name = "vook-rails-1-private-subnet-1a"
+    Name = "vook-rails-2-private-subnet-1a"
   }
 }
 
 # ---------------------------
 # Internet Gateway
 # ---------------------------
-resource "aws_internet_gateway" "vook_rails_1_igw" {
-  vpc_id = aws_vpc.vook_rails_1_vpc.id
+resource "aws_internet_gateway" "vook_rails_2_igw" {
+  vpc_id = aws_vpc.vook_rails_2_vpc.id
   tags = {
-    Name = "vook-rails-1-igw"
+    Name = "vook-rails-2-igw"
   }
 }
 
@@ -46,35 +46,35 @@ resource "aws_internet_gateway" "vook_rails_1_igw" {
 # Route table
 # ---------------------------
 # Route table作成 public
-resource "aws_route_table" "vook_rails_1_public_route" {
-  vpc_id = aws_vpc.vook_rails_1_vpc.id
+resource "aws_route_table" "vook_rails_2_public_route" {
+  vpc_id = aws_vpc.vook_rails_2_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.vook_rails_1_igw.id
+    gateway_id = aws_internet_gateway.vook_rails_2_igw.id
   }
   tags = {
-    Name = "vook-rails-1-public-route"
+    Name = "vook-rails-2-public-route"
   }
 }
 
 # SubnetとRoute tableの関連付け public
-resource "aws_route_table_association" "vook_rails_1_public_route" {
-  subnet_id      = aws_subnet.vook_rails_1_public_subnet_1a.id
-  route_table_id = aws_route_table.vook_rails_1_public_route.id
+resource "aws_route_table_association" "vook_rails_2_public_route" {
+  subnet_id      = aws_subnet.vook_rails_2_public_subnet_1a.id
+  route_table_id = aws_route_table.vook_rails_2_public_route.id
 }
 
 # Route table作成 private
-resource "aws_route_table" "vook_rails_1_private_route" {
-  vpc_id = aws_vpc.vook_rails_1_vpc.id
+resource "aws_route_table" "vook_rails_2_private_route" {
+  vpc_id = aws_vpc.vook_rails_2_vpc.id
   tags = {
-    Name = "vook-rails-1-private-route"
+    Name = "vook-rails-2-private-route"
   }
 }
 
 # SubnetとRoute tableの関連付け private
-resource "aws_route_table_association" "vook_rails_1_private_route" {
-  subnet_id      = aws_subnet.vook_rails_1_private_subnet_1a.id
-  route_table_id = aws_route_table.vook_rails_1_private_route.id
+resource "aws_route_table_association" "vook_rails_2_private_route" {
+  subnet_id      = aws_subnet.vook_rails_2_private_subnet_1a.id
+  route_table_id = aws_route_table.vook_rails_2_private_route.id
 }
 
 # ---------------------------
@@ -97,12 +97,12 @@ locals {
 }
 
 # Security Group作成
-resource "aws_security_group" "vook_rails_1_sg" {
-  name        = "vook-rails-1-sg"
+resource "aws_security_group" "vook_rails_2_sg" {
+  name        = "vook-rails-2-sg"
   description = "For EC2 Linux"
-  vpc_id      = aws_vpc.vook_rails_1_vpc.id
+  vpc_id      = aws_vpc.vook_rails_2_vpc.id
   tags = {
-    Name = "vook-rails-1-sg"
+    Name = "vook-rails-2-sg"
   }
 
   # インバウンドルール
